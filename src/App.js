d32077d6037425f33d05622c4911397a07fc69a5
mrcruz117 <mrcruz1989@gmail.com>
2021-12-07 09:28:27 -0500

    set everything up for JWT backend playground

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import RequestTester from "./components/RequestTester";
 
 function App() {
+  document.body.style = "background: grey;";
   return (
     <div>
       <RequestTester />
--- src/components/RequestTester.js
+++ src/components/RequestTester.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Card } from "@mui/material";
+import axios from "axios";
 
 export default function RequestTester() {
+  const [response, set_response] = useState("Test info");
+
+  const makeRequest = () => {
+    // add https request for JWT testing later
+
+    set_response("button working");
+  };
   return (
     <div
       style={{
@@ -19,9 +27,14 @@ export default function RequestTester() {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
+          flexDirection: "column",
         }}
       >
-        <Button variant="contained">GO</Button>
+        <Button onClick={makeRequest} variant="contained">
+          GO
+        </Button>
+        <br />
+        <div style={{ marginTop: "10px" }}>{response}</div>
       </Card>
     </div>
   );
